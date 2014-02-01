3e4ae6a5255918212aef7b345d2022dccf1344c0
Dr.Nattapon Chatprechakul <nattapon@chatprechakul.info>
2014-02-01 22:19:25 +0700

    Finish chapter 3 tutorial

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
   	it "should have the content 'Sample App'" do
   		visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "StaticPages" do
   	it "should have the title 'Home'" do
   		visit '/static_pages/home'
   		page.should have_selector('title',
-  							:text=> "Ruby on Rails Tutorial Sample App | Home")
+  							:text=> "#{base_title} | Home")
   	end
   	
   end
@@ -23,7 +26,7 @@ describe "StaticPages" do
   	it "should have the title 'Help'" do
   		visit '/static_pages/help'
   		page.should have_selector('title',
-  							:text=> "Ruby on Rails Tutorial Sample App | Help")
+  							:text=> "#{base_title} | Help")
   	end  	
   end
   
@@ -36,6 +39,18 @@ describe "StaticPages" do
   	it "should have the title 'About Us'" do
   		visit '/static_pages/about'
   		page.should have_selector('title',
-  							:text=> "Ruby on Rails Tutorial Sample App | About Us")
+  							:text=> "#{base_title} | About Us")
   	end  	
+  describe "Contact page" do
+  	it "should have the content 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_content('Contact')
+  	end
+  	
+  	it "should have the title 'Contact'" do
+  		visit '/static_pages/contact'
+  		page.should have_selector('title',
+  							:text=> "#{base_title} | Contact")
+  	end
+  end
 end
